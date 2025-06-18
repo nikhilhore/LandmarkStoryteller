1256e7d56c46fd0992f67e752793f040fb85fcb6
Nikhil Hore <horenikhil5@gmail.com>
2025-06-18 22:29:56 +0530

    Initial Commit

---
+++ LandmarkStorytellerTests/LandmarkStorytellerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkStorytellerTests.swift
+//  LandmarkStorytellerTests
+//
+//  Created by Nikhil Hore on 18/06/25.
+//
+
+import Testing
+@testable import LandmarkStoryteller
+
+struct LandmarkStorytellerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
